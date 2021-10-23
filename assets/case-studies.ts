96993f102f56c9880c192fa1ab589c9e6486f7cb
darrensapalo <darren.sapalo@gmail.com>
2021-10-23 21:38:07 +0800

    2 files modified, 4 added, 2 deleted

---
+++ assets/case-studies.ts
@@ -0,0 +1,9 @@
+import { CaseStudy } from '../src/domain';
+
+export default [
+  {
+    name: 'Sample',
+    description: 'A sample case study',
+    url: 'https://www.google.com',
+  },
+] as CaseStudy[];
---
+++ src/casestudies/domain.ts
@@ -0,0 +1,5 @@
+export type CaseStudy = {
+  url: string;
+  name: string;
+  description: string;
+};
